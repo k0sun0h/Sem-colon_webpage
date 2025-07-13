f7e0f1d3304ea0e6e9b4c6e4a099f6d8f8a63c2a
김선혜 <kimsunhye1225@gmail.com>
2025-07-13 17:38:15 +0900

    [김선헤] - App 레이아웃에 상세 주석 추가 - Introduce 페이지 라우팅 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 // 전체 레이아웃 (앱 전체를 감싸는 컴포넌트)
 // Header, Footer, 페이지별 Route가 설정됨
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// 이 컴포넌트들이 모든 페이지에 공통으로 들어감 (전역 레이아웃)
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+// /pages/Introduce.js 파일에서 Introduce 페이지 컴포넌트를 가져옴
 import Introduce from './pages/Introduce';
 
 function App() {
