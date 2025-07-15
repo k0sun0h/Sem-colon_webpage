fa9bd6d7f9345ffec1d85f59970ea59ffa0364b6
김선혜 <kimsunhye1225@gmail.com>
2025-07-15 20:37:19 +0900

    [김선헤] - 스크롤바 정상화

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -9,15 +9,15 @@ function VideoBackground() {
         videoId="XNf1lWwc-Cw"
         opts={{
           playerVars: {
-            autoplay: 1,
-            controls: 0,
-            showinfo: 0,
-            modestbranding: 1,
-            loop: 1,
-            mute: 1,
-            playlist: 'XNf1lWwc-Cw',
-            start: 0,
-            end: 120
+            autoplay: 1, // 자동 재생
+            controls: 0, // 컨트롤러 숨기기
+            showinfo: 0, // 영상 정보 숨기기
+            modestbranding: 1, // 브랜드 로고 숨기기
+            loop: 1, // 반복 재생
+            mute: 1, // 음소거
+            playlist: 'XNf1lWwc-Cw', // 반복 재생을 위한 플레이리스트 설정
+            start: 0, // 시작 시간
+            end: 119, // 종료 시간 (119초)
           },
         }}
         className="video-iframe"
